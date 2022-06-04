4ffeec00048f4d041fc75c343755ccaa9b479c5b
DoosanD <davicdusan@gmail.com>
2022-06-04 17:47:57 +0200

    test

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
 const express = require("express")();
-const app = express;
+const app = express();
 const PORT = 8000;
 
 const rappers = {
-  "21 savage": { age: 29, name: "bin", location: "london" },
-  " chance the dog": { age: 29, name: "chancelor", location: "usa" },
-  "unknown ": { age: 0, name: "unknown", location: "unknown" },
+  "21 savage": {
+    age: 29,
+    birthName: "Shéyaa Bin Abraham-Joseph",
+    birthLocation: "London, England",
+  },
+  "chance the rapper": {
+    age: 29,
+    birthName: "Chancelor Bennett",
+    birthLocation: "Chicago, Illinois",
+  },
+  dylan: {
+    age: 29,
+    birthName: "Dylan",
+    birthLocation: "Dylan",
+  },
 };
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api:name", (request, response) => {
-  const rapperName = request.params.name.toLowerCase();
-  if (rappers[rapperName]) {
-    response.json(rappers[rapperName]);
+app.get("/api/:rapperName", (request, response) => {
+  const rappersName = request.params.rapperName.toLowerCase();
+  if (rappers[rappersName]) {
+    response.json(rappers[rappersName]);
   } else {
-    response.json(rappers["unknown"]);
+    response.json(rappers["dylan"]);
   }
 });
 
 app.listen(PORT, () => {
-  console.log("its alive");
+  console.log(`The server is running on port ${PORT}! You better go catch it!`);
 });
