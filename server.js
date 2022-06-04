31e318c6c49b2916eca5ecc4f4b3c0d2a068f490
DoosanD <davicdusan@gmail.com>
2022-06-04 19:57:42 +0200

    update

--- server.js
+++ server.js
@@ -1,78 +1,26 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
+const express = require("express")();
+const app = express;
+const PORT = 8000;
 
-var db, collection;
+const rappers = {
+  "21 savage": { age: 29, name: "bin", location: "london" },
+  " chance the dog": { age: 29, name: "chancelor", location: "usa" },
+  "unknown ": { age: 0, name: "unknown", location: "unknown" },
+};
 
-const url =
-  "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
-
-app.listen(3000, () => {
-  MongoClient.connect(
-    url,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (error, client) => {
-      if (error) {
-        throw error;
-      }
-      db = client.db(dbName);
-      console.log("Connected to `" + dbName + "`!");
-    }
-  );
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
 });
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  db.collection("messages")
-    .find()
-    .toArray((err, result) => {
-      if (err) return console.log(err);
-      res.render("index.ejs", { messages: result });
-    });
+app.get("/api", (request, response) => {
+  //   const rapperName = request.params.name.toLowerCase();
+  //   if (rappers[rapperName]) {
+  response.json(rappers);
+  //   } else {
+  //     response.json(rappers["unknown"]);
+  //   }
 });
 
-app.post("/messages", (req, res) => {
-  db.collection("messages").insertOne(
-    { name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown: 0 },
-    (err, result) => {
-      if (err) return console.log(err);
-      console.log("saved to database");
-      res.redirect("/");
-    }
-  );
-});
-
-app.put("/messages", (req, res) => {
-  db.collection("messages").findOneAndUpdate(
-    { name: req.body.name, msg: req.body.msg },
-    {
-      $set: {
-        thumbUp: req.body.thumbUp + 1,
-      },
-    },
-    {
-      sort: { _id: -1 },
-      upsert: true,
-    },
-    (err, result) => {
-      if (err) return res.send(err);
-      res.send(result);
-    }
-  );
-});
-
-app.delete("/messages", (req, res) => {
-  db.collection("messages").findOneAndDelete(
-    { name: req.body.name, msg: req.body.msg },
-    (err, result) => {
-      if (err) return res.send(500, err);
-      res.send("Message deleted!");
-    }
-  );
+app.listen(process.env.PORT || PORT, () => {
+  console.log("its alive");
 });
