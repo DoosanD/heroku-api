a738ce3c52556d662b6dc27189079a38bffdf19d
DoosanD <davicdusan@gmail.com>
2022-06-04 17:35:37 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")();
+const app = express;
+const PORT = 8000;
+
+const rappers = {
+  "21 savage": { age: 29, name: "bin", location: "london" },
+  " chance the dog": { age: 29, name: "chancelor", location: "usa" },
+  "unknown ": { age: 0, name: "unknown", location: "unknown" },
+};
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api:name", (request, response) => {
+  const rapperName = request.params.name.toLowerCase();
+  if (rappers[rapperName]) {
+    response.json(rappers[rapperName]);
+  } else {
+    response.json(rappers["unknown"]);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("its alive");
+});
